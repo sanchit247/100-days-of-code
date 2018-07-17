e42d693bf7d3857afcc3bd6c7e5b64ea41b8f6ee
sanchit247 <30755370+sanchit247@users.noreply.github.com>
2018-07-18 04:55:00 +0530

    print largest subarray

---
+++ code/day7/largestSubArr.py
@@ -0,0 +1,21 @@
+#  Find the largest sub-array that can be formed by picking consecutive integers.
+
+n=[2,5,7,0,1,3,4,8,9,10]
+m=[]
+for i in range(len(n)):
+    if(i not in n):
+        break
+    else:
+        m.append(i)
+# m prints element
+l=[]
+for i in range(len(m)):
+    for j in range(len(n)):
+        if(m[i] == n[j]):
+           l.append(j) 
+# l prints indexes
+l.sort()
+s=[]
+for i in l:
+    s.append(n[i])
+print(s)
